492c89c1716141405d8695c024f25e78a1f80d22
Luc Perkins <lucperkins@gmail.com>
2023-06-05 13:10:41 +0200

    Add CI job to verify that the action works

---
+++ flake.nix
@@ -0,0 +1,24 @@
+# This flake.nix is only here to provide a flake.lock to check in CI
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          pkgs.mkShell {
+            packages = (with pkgs; [
+              nixpkgs-fmt
+            ]);
+          };
+      });
+    };
+}
